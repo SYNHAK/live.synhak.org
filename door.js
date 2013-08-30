3a1232fb0ced3eadec6adfa3f559b83fa787d7d3
Torrie Fischer <tdfischer@hackerbots.net>
2013-08-29 20:10:15 -0400

    Implement oembed

---
+++ door.js
@@ -0,0 +1,30 @@
+function onDoorSensorPolled(data) {
+  console.log(data);
+  updateSign(data.value);
+}
+
+function updateSign(isOpen) {
+  $('#door-state').removeClass('door-state-checking');
+  $('#door-state').removeClass('door-state-error');
+  if (isOpen) {
+    $('#door-state').addClass('door-state-open');
+  } else {
+    $('#door-state').addClass('door-state-closed');
+  }
+}
+
+function updateSignError() {
+  $('#door-state').removeClass('door-state-checking');
+  $('#door-state').removeClass('door-state-open');
+  $('#door-state').removeClass('door-state-closed');
+  $('#door-state').addClass('door-state-error');
+  $('#door-state-timestamp').text("Sysadmins are already on it!");
+}
+
+$(document).ready(function () {
+  $.ajax({
+    url: "https://synhak.org/auth/sensors/3.json", 
+    success: onDoorSensorPolled,
+    error: updateSignError,
+  })
+});
