dfa2d3b747168605ccb0a018143a1f6c116dfe66
Torrie Fischer <tdfischer@hackerbots.net>
2013-10-18 22:51:42 -0400

    add jukebox status

--- live.js
+++ live.js
@@ -1,9 +1,22 @@
+function updateJukebox(jsonData) {
+  data = JSON.parse(jsonData);
+  $("#now-playing-text").text(data.song.title+" by "+data.song.artist);
+}
+
+function onJukeboxSensorPolled(data) {
+  updateJukebox(data.value);
+}
+
+function updateJukeboxError() {
+  $("#now-playing-text").text("Couldn't load!");
+}
+
 $(document).ready(function () {
   $.ajax({
-    url: "https://synhak.org/auth/sensors/3.json", 
-    success: onDoorSensorPolled,
-    error: updateSignError,
-  })
+    url: "https://synhak.org/auth/sensors/4.json",
+    success: onJukeboxSensorPolled,
+    error: updateJukeboxError,
+  });
   $('#hackercam').error(function () {
     this.src = "http://webcam/mjpg/video.mjpg";
   });
